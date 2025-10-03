2d83e5d2bd0a4540a732fa79c3f4c42ac1175ab7
Jake <jaketurnick@gmail.com>
2025-10-03 13:58:43 -0400

    Page directory with blank componenets

---
+++ Pages/Accounts.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace HPM_Companion.Pages;
+
+public partial class Accounts : UserControl
+{
+    public Accounts()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Pages/Fingerprints.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace HPM_Companion.Pages;
+
+public partial class Fingerprints : UserControl
+{
+    public Fingerprints()
+    {
+        InitializeComponent();
+    }
+}
